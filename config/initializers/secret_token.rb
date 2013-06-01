8852edd3d71410aff4cd5a30e72e5293e52792d0
Angela Pedroso Tonon <angptonon@gmail.com>
2013-06-01 11:47:35 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngelaTonon::Application.config.secret_token = 'd279f6e216ff46068d12c68a85e9068fefae45b882f62bd96bf4141c3f11eaa56b94eca1464ac387080a9f6631e5ae92ade867599d9908d6e370ded1fc467e73'
